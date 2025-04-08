d4b89d10dcf5d6f46200fb22fc4bebe336acb268
Achraf Nouri <achrafnourix@gmail.com>
2025-04-08 03:45:42 +0100

    Add files via upload

---
+++ Thisnet/login.php
@@ -0,0 +1,14 @@
+<?php
+if ($_POST['username']=='achraf_nr' and $_POST['password']==''){
+    header('location: thisnet.html');
+}
+else{
+    header('location: error.html')
+}
+if ($_POST['username']=='aymane_nouri' and $_POST['password']=='aymaner198213dfr'){
+    header('location: thisnet.html');
+}
+else{
+    header('location: error.html')
+}
+?>
